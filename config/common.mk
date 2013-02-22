49c39970c1b9c0fffa46c585d10b37fbffedbf5c
flappjaxxx <flappjaxxx@gmail.com>
2013-02-22 18:54:22 -0500

    FJKang!

--- config/common.mk
+++ config/common.mk
@@ -219,7 +219,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := FJKang
     CM_EXTRAVERSION :=
 endif
 
