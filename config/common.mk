4b9ac6a4ae4d2c12e75adf236235de79976e1215
flappjaxxx <flappjaxxx@gmail.com>
2014-01-01 22:59:07 -0500

    flappjaxxx is my name yo!

--- config/common.mk
+++ config/common.mk
@@ -34,10 +34,10 @@ endif
 
 ifdef CM_NIGHTLY
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmodnightly
+    ro.rommanager.developerid=flappjaxxx
 else
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmod
+    ro.rommanager.developerid=flappjaxxx
 endif
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
